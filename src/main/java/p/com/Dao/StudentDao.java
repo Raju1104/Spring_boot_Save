04875ac30e3c267f8d1d5c97e0de3d64ed42b25c
raju1104 <ichalekar777@gmail.com>
2023-08-25 18:44:19 +0530

    FindByName And FindAll method Added

--- src/main/java/p/com/Dao/StudentDao.java
+++ src/main/java/p/com/Dao/StudentDao.java
@@ -1,9 +1,12 @@
 package p.com.Dao;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import p.com.Entity.Student;
 
-public interface StudentDao  extends JpaRepository<Student, Integer>{
+public interface StudentDao extends JpaRepository<Student, Integer> {
+
+	List<Student> findByfname(String fname);
 
 }
--- src/main/java/p/com/service/Studentservice.java
+++ src/main/java/p/com/service/Studentservice.java
@@ -1,5 +1,6 @@
 package p.com.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,29 +8,42 @@ import org.springframework.stereotype.Component;
 
 import p.com.Dao.StudentDao;
 import p.com.Entity.Student;
+
 @Component
 public class Studentservice {
 	@Autowired
 	private StudentDao studentDao;
 
 	public void add(Student student) {
-		
+
 		studentDao.save(student);
 	}
 
-	 public Student findbyid(int id) {
-	        Optional<Student> optional = studentDao.findById(id);
-	        
-	        if (optional.isPresent()) {
-	            return optional.get();
-	        } else {
-	            throw new RuntimeException("Student Not Found.....: " + id);
-	        }
-	
-}
+	public Student findbyid(int id) {
+		Optional<Student> optional = studentDao.findById(id);
+
+		if (optional.isPresent()) {
+			return optional.get();
+		} else {
+			throw new RuntimeException("Student Not Found.....: " + id);
+		}
+
+	}
 
 	public Student deletebyid(int id) {
-	studentDao.deleteById(id);
-	return null;
-		
-	}}
+		studentDao.deleteById(id);
+		return null;
+
+	}
+
+	public List<Student> findAll() {
+		List<Student> list = studentDao.findAll();
+		return list;
+	}
+
+	public List<Student> findStudentByName(String fname) {
+		List<Student> list = studentDao.findByfname(fname);
+		return list;
+	}
+	
+}
