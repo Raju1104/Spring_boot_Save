beb2d456d9b5cf7e92f1d4fd5911f304bbb10444
Raju1104 <ichal@DESKTOP-7BUJ42N>
2023-08-28 02:23:09 -0700

    Swagger Documentation Added

--- src/main/java/p/com/SaveApplicationstarter.java
+++ src/main/java/p/com/SaveApplicationstarter.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
-@PropertySource(value="classpath:db.properties")
+@PropertySource(value="classpath:p.properties")
 @SpringBootApplication
 public class SaveApplicationstarter {
 
