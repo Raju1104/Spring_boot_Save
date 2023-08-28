a11e30225b5dc386b7f8e4973d094649305951de
Raju1104 <ichal@DESKTOP-7BUJ42N>
2023-08-28 12:06:53 -0700

    Validation Added

--- src/main/java/p/com/Controller/Homecontroller.java
+++ src/main/java/p/com/Controller/Homecontroller.java
@@ -3,6 +3,7 @@ package p.com.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,25 +12,34 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import p.com.Entity.Student;
-import p.com.service.Studentservice;
+import p.com.service.Studentservice; 
 
 @RestController
 public class Homecontroller {
 
 	@Autowired
 	private Studentservice studentservice;
-
+	
+	@Value("$(errormsg)")
+     private String msg;
+	
+	@Value("$(message)")
+    private String emsg;
+	
 	@PostMapping("/student")
 	public String add(@RequestBody Student student) {
 		studentservice.add(student);
-
+		System.out.println(emsg);
 		return "Data Added Successfully";
 
 	}
 
 	@GetMapping("/student/id/{id}")
 	public Student findStudentById(@PathVariable int id) {
+		System.out.println(msg);
+		
 		return studentservice.findbyid(id);
+		
 	}
 
 	@DeleteMapping("/student/id/{id}")
